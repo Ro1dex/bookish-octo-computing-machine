d0a6a7cf7dcb1ff401ab7510db80cefa457c4936
Ro1dex <sanhcez58@gmail.com>
2023-02-12 10:59:10 +0200

    cover

--- src/test/java/MaxServicesTest.java
+++ src/test/java/MaxServicesTest.java
@@ -3,12 +3,21 @@ import org.junit.jupiter.api.Test;
 
 public class MaxServicesTest {
     @Test
-    public void shouldFindMax() {
+    public void shouldFindMaxB() {
         MaxServices services = new MaxServices();
         int a = 0;
         int b = 1;
          int exp = b;
          int act = services.max(a, b);
+        Assertions.assertEquals(exp, act);
+    }
+    @Test
+    public void shouldFindMaxA() {
+        MaxServices services = new MaxServices();
+        int a = 1;
+        int b = 0;
+        int exp = a;
+        int act = services.max(a, b);
         Assertions.assertEquals(exp, act);
     }
 
